08e7dd9c102d90d4fb88272724cc151daa1b9cd2
Shakhboz <shakhboztursunboev@gmail.com>
2025-10-16 10:46:21 +0500

    refactor(basic-4.5): simplify list iteration using for...of

--- app.js
+++ app.js
@@ -142,15 +142,12 @@ const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
 
 //cycle over incompleteTaskHolder ul list items
 //for each list item
-for (let i = 0; i < incompleteTaskHolder.children.length; i++) {
-	//bind events to list items chldren(tasksCompleted)
-	bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted)
+for (const li of incompleteTaskHolder.children) {
+  bindTaskEvents(li, taskCompleted)
 }
 
-//cycle over completedTasksHolder ul list items
-for (let i = 0; i < completedTasksHolder.children.length; i++) {
-	//bind events to list items chldren(tasksIncompleted)
-	bindTaskEvents(completedTasksHolder.children[i], taskIncomplete)
+for (const li of completedTasksHolder.children) {
+  bindTaskEvents(li, taskIncomplete)
 }
 
 // Issues with usability don't get seen until they are in front of a human tester.
